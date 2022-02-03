9314e80b7662f3e1e1e059daa7399fe4db94de25
Saref111 <saref012@gmail.com>
2022-02-03 11:14:16 +0200

    fixed const issue

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const createStore = function (reducer, initiaSstate) {
-    const currentReducer = reducer
-    const currentState = initiaSstate
+const createStore = function (reducer, initiaState) {
+    let currentReducer = reducer
+    let currentState = initiaState
 
     return {
         getState() {
@@ -12,3 +12,26 @@ const createStore = function (reducer, initiaSstate) {
         }
     }
 }
+
+// tests
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'TEST':
+            return {...state, ...action.payload}
+    }
+}
+
+const store = createStore(reducer, {value: 0})
+
+console.log(store);
+
+const st1 = store.getState()
+console.log(st1)
+
+store.dispatch({type: 'TEST', payload: {
+    value: '1'
+}})
+const st2 = store.getState()
+console.log(st1 === st2)
+
