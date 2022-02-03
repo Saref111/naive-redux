14fc8eeb1b92f72ef9a447a198701e0d54a81d93
Saref111 <saref012@gmail.com>
2022-02-03 11:21:19 +0200

    removed tests from index

--- index.js
+++ index.js
@@ -18,34 +18,3 @@ const createStore = function (reducer, initiaState) {
     }
 }
 
-// tests
-
-const reducer = (state, action) => {
-    switch (action.type) {
-        case 'TEST':
-            return {...state, ...action.payload}
-    }
-}
-
-const store = createStore(reducer, {value: 0})
-
-console.log(store);
-
-const st1 = store.getState()
-console.log(st1)
-
-store.dispatch({type: 'TEST', payload: {
-    value: '1'
-}})
-
-store.subscribe(() => {
-    console.log('changed@!!!');
-})
-
-const st2 = store.getState()
-console.log(st1 === st2)
-
-
-store.dispatch({type: 'TEST', payload: {
-    value: '122'
-}})
